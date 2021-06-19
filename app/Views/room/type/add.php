30a02a8cee200b0d4a001eef0125bc2f0fc8f4ea
Earl Janiel Compra <compranicles@gmail.com>
2021-06-19 12:40:47 +0800

    View Changes for Room Type

--- app/Views/room/type/add.php
+++ app/Views/room/type/add.php
@@ -7,30 +7,39 @@
 <div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-4">
-            <h4 class="mb-3 text-center">Add Room Type</h4>
-            <?= form_open(); ?>
-            <form>
-                <div class="form-floating mb-3">
-                    <input type="text" name="name" id="name" placeholder="e.g. Single Room, etc." class="form-control">
-                    <label for="name">Name</label>
+            <div class="card">
+                <div class="card-header">
+                    <h4>
+                        Add Room Type
+                        <a href="<?= base_url().'/room/type'?>" class="btn btn-danger float-end">BACK</a>
+                    </h4>
                 </div>
-                <div class="form-floating mb-3">
-                    <textarea name="description" class="form-control" placeholder="e.g. Room Size, Features" id="description" style="height: 100px"></textarea>
-                    <label for="description">Description</label>  
-                </div>
-                <div class="form-floating mb-3">
-                    <input type="number" name="price" id="price" placeholder="(PHP)" step="0.01" min="0" max="99999" class="form-control">
-                    <label for="price">Price (PHP)</label>
-                </div>
-                <div class="form-floating mb-3">
-                    <input type="number" name="max_guests" id="max_guests" placeholder="Minimum 1" min="1" max="5" class="form-control">
-                    <label for="max_guests">Maximum number of guests</label>
-                </div>
-                <div class="form-group mt-2 text-center">
-                    <input type="submit" value="Save" class="btn btn-success">
-                </div>
-            </form>
-            <?= form_close(); ?>
+                <div class="card-body">
+                    <?= form_open(); ?>
+                        <form>
+                            <div class="form-floating mb-3">
+                                <input type="text" name="name" id="name" placeholder="e.g. Single Room, etc." class="form-control">
+                                <label for="name">Name</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <textarea name="description" class="form-control" placeholder="e.g. Room Size, Features" id="description" style="height: 100px"></textarea>
+                                <label for="description">Description</label>  
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="number" name="price" id="price" placeholder="(PHP)" step="0.01" min="0" max="99999" class="form-control">
+                                <label for="price">Price (PHP)</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="number" name="max_guests" id="max_guests" placeholder="Minimum 1" min="1" max="5" class="form-control">
+                                <label for="max_guests">Maximum number of guests</label>
+                            </div>
+                            <div class="form-group mt-2 text-center d-grid gap-2">
+                                <input type="submit" value="Save" class="btn btn-success">
+                            </div>
+                        </form>
+                    <?= form_close(); ?>
+                    </div>
+            </div>
         </div>
     </div>
 </div>
--- app/Views/room/type/index.php
+++ app/Views/room/type/index.php
@@ -2,8 +2,7 @@
 
 <?= $this->section("content") ?>
 
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/jq-3.3.1/dt-1.10.25/datatables.min.css"/>
-<script type="text/javascript" src="https://cdn.datatables.net/v/bs5/jq-3.3.1/dt-1.10.25/datatables.min.js"></script>
+
 
 
 <div class="container mt-4">
