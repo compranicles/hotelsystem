563b2fc160dd5f870c55b3d6d04772a536aac3c6
kenpenndmnl <65898289+kenpenndmnl@users.noreply.github.com>
2021-06-21 14:43:43 +0800

    commit_pan_2
    -Home page updated (when navlink is active)
    
    (Rooms, Room Types, Payment Types)
    -Alerts for CRUD
    -Modals for delete actions

--- app/Controllers/Room.php
+++ app/Controllers/Room.php
@@ -8,6 +8,12 @@ class Room extends BaseController
     public function __construct() {
         helper('form');
 		$this->roomModel = new RoomModel();
+<<<<<<< Updated upstream
+=======
+		$this->roomTypeModel = new RoomTypeModel();
+		$this->roomStatusModel = new RoomStatusModel();
+		$this->session = \Config\Services::session();
+>>>>>>> Stashed changes
     }
 
 	public function index()
@@ -26,6 +32,7 @@ class Room extends BaseController
 			];
 
 			if($this->roomModel->save($data) === true){
+<<<<<<< Updated upstream
 				return redirect()->to(base_url().'/room/add');
 			}
 		}
@@ -36,5 +43,65 @@ class Room extends BaseController
 		if($this->roomModel->where('id',$id)->delete()){
 			return redirect()->to(base_url().'/room');
 		}
+=======
+				$this->session->setTempdata('success', 'Added Successfully!', 3);
+				return redirect()->to(base_url().'/room');
+			} else {
+				$this->session->setTempdata('error', 'Adding Failed', 3);
+			}
+		}
+		return view('room/add', $data);
+	}
+
+	public function edit($id=null){
+		date_default_timezone_set('Asia/Manila');
+		$data['room'] = $this->roomModel->find($id);
+		$data['room_types'] = $this->roomTypeModel->findAll();
+		$data['room_status'] = $this->roomStatusModel->findAll();
+
+		if($this->request->getMethod()=='post'){
+			$room = $data['room'];
+			$oldPhotoName = $room['photo'];
+
+			$file = $this->request->getFile('photo');
+
+			if($file->isValid() && ! $file->hasMoved()){
+				if(file_exists("uploads/".$oldPhotoName)){
+					unlink("uploads/".$oldPhotoName);
+				}
+				$photoName = $file->getRandomName();
+				$file->move('uploads/', $photoName);
+			}
+			else{
+				$photoName = $room['photo'];
+			}
+			$data =[
+				'name' => $this->request->getVar('name'),
+				'floor' => $this->request->getVar('floor'),
+				'photo' => $photoName,
+				'room_type_id' => $this->request->getVar('room_type_id'),
+				'room_status_id' => $this->request->getVar('room_status_id'),
+			];
+
+			if($this->roomModel->update($id, $data) === true){
+				$this->session->setTempdata('success', 'Updated Successfully!', 3);
+				return redirect()->to(base_url().'/room');
+			} else {
+				$this->session->setTempdata('error', 'Update Failed!', 3);
+			}
+		}
+
+		return view('room/edit',$data);
+	}
+
+	public function delete($id=null){
+		if($this->roomModel->where('room_id',$id)->delete() === true){
+			$this->session->setTempdata('success', 'Deleted Successfully!', 3);
+			return redirect()->to(base_url().'/room');
+		}
+		else{
+			$this->session->setTempdata('error', 'Delete Failed!', 3);
+		}
+>>>>>>> Stashed changes
 	}
 }
---
+++ app/Views/homepage/header.php
@@ -0,0 +1,29 @@
+<header class="header">
+    <div class="header_content d-flex flex-column align-items-center justify-content-lg-end justify-content-center">
+
+        <div class="logo_container">
+        <a href="#">
+            <img class="logo" src="cavella_logo.png" alt="">
+        </a>
+        </div>
+        <!-- Main Nav -->
+        <nav class="main_nav">
+            <ul class="d-flex flex-row align-items-center justify-content-start">
+                <li><a class="active" href="/">Home</a></li>
+                <li><a href="#">Rooms</a></li>
+                <li><a href="#">About us</a></li> 
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <!-- Hamburger Button -->
+            <!-- Navigation in Right Part of the Header -->
+            <div class="right_nav">
+                <ul class="d-flex flex-row align-items-center justify-content-start">
+
+                        <li><a href="#">Login</a> | <a href="#">Register</a></li>
+
+                </ul>
+            </div>
+            <div class="hamburger"><i class="fa fa-bars" aria-hidden="true"></i></div>
+        </nav>
+    </div>
+</header>
