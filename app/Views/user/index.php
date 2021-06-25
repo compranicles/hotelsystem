a717e391084d6b0b8c9ee10d58224e5fe0c3aa19
Earl Janiel Compra <compranicles@gmail.com>
2021-06-25 09:47:50 +0800

    Update index.php

--- app/Views/user/index.php
+++ app/Views/user/index.php
@@ -36,7 +36,6 @@
                                 <tr>
                                     <th>User Id</th>
                                     <th>User Name</th>
-                                    <th>User Data</th>
                                     <th>User Roles</th>
                                     <th>Actions</th>
                                 </tr>
@@ -45,9 +44,8 @@
                                 <?php foreach($users as $user): ?>
                                 <tr>
                                     <td><?= $user['user_id'] ?></td>
-                                    <td><?= $user['first_name']." ".$user['last_name']?></td>
                                     <td>
-                                        <a href="<?= base_url().'/user/view/'.$user['user_id'] ?>" class="btn btn-primary">View Data</a>
+                                        <a href="<?= base_url().'/user/view/'.$user['user_id'] ?>" class="link-primary fw-bold"><?= strtoupper($user['last_name'].", ".$user['first_name'])?></a>
                                     </td>
                                     <td>
                                         <a href="<?= base_url().'/user/role/'.$user['user_id'] ?>" class="btn btn-secondary">Manage Roles</a>
