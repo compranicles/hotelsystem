c37dc3e25e38c84574479a4f8b22f8563b21add8
kenpenndmnl <65898289+kenpenndmnl@users.noreply.github.com>
2021-07-01 11:27:50 +0800

    Update
    - Reservation controller update

--- app/Controllers/Reservation.php
+++ app/Controllers/Reservation.php
@@ -1,73 +1,87 @@
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-body">
-			<img class="logo-forpay mx-auto" src="/cavella_logo.png" alt="">
-			<h3 class="text-center">Reservation Details</h3><br>
-			<div class="row">
-			<?php foreach ($forPayment as $row): 
-				$arrival = new DateTime($row['arrival_date']);
-				$departure = new DateTime($row['departure_date']);
+<?php
 
-				$nights = $departure->diff($arrival)->format("%a");
-				
-				$total = $row['price'] * $nights;
-			?>
-			
-			<h5 class="name_payment mb-3"><?= $row['first_name']." ".$row['last_name']; ?></h5>
-			<div class="col-md-12">
-				<p class="mb-1">Reservation Date: &emsp;&emsp;&emsp;<?= $row['date_created']; ?></p>
-				<p class="mb-1">Arrival Date: &emsp;&emsp;&emsp;&nbsp;&emsp;&emsp;<?= $row['arrival_date']; ?></p>
-				<p class="mb-3">Departure Date: &emsp;&ensp;&emsp;&emsp;<?= $row['departure_date']; ?></p>
-			</div>
+namespace App\Controllers;
+use App\Models\RoomTypeModel;
+use App\Models\RoomModel;
+use App\Models\ReservationModel;
+use App\Models\BookingModel;
 
-			<table class="table">
-				<thead>
-					<tr>
-					<th scope="col">#</th>
-					<th scope="col">Amount</th>
-					<th scope="col">Reservation</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-					<th scope="row">1</th>
-					<td>&#8369; <?= number_format($row['price'], 2); ?><br>
-						<?php if ($nights >= 2): ?>
-							x (<?= $nights; ?>) nights
-						<?php endif; ?>
-					</td>
-					<td>
-						<table>
-							<tr>
-								<td>Room <?= $row['room_name']; ?></td>
-							</tr>
-							<tr>
-								<td>Floor #<?= $row['floor']; ?></td>
-							</tr>
-							<tr>
-								<td><?= $row['room_type_name']; ?></td>
-							</tr>
-						</table>
-					</td>
-					</tr>
-				</tbody>
-			</table>
-			</div>
-			<p class="amount"><strong>Total Amount: </strong> &#8369; <?= number_format($total, 2); ?></p>
-			<?php endforeach; ?>
+class Reservation extends BaseController
+{
+    public function __construct() {
+		helper('url');
+        helper('form');
+		$this->roomType = new RoomTypeModel();
+		$this->room = new RoomModel();
+    }
+
+	public function index()
+	{
+		date_default_timezone_set('Asia/Manila');
+		return view('reservation/index');
+	}
+
+	public function showroom(){
+		date_default_timezone_set('Asia/Manila');
+		$data['room_types'] = $this->roomType->findAll();
+
+
+		if($this->request->getMethod()=='post'){
 			
-			<div class="form_select_paymentType">
-				<p><strong>Payment Type:</strong>
-				<select class="form-select rounded-0 mt-1" name="payment_type" id="select_payment">
-				<?php foreach ($forPaymentType as $type): ?>
-					<option selected disabled>--Select--</option>
-					<option value="<?= $type['payment_type_id']; ?>"><?= $type['name']; ?></option>
-					<?php endforeach; ?>
-				</select>
-				</p>
-			</div>
+			$start_date = $this->request->getVar('arrival_date');
+			$end_date = $this->request->getVar('departure_date');
+			$room_type_id = $this->request->getVar('room_type');
+			$guests = $this->request->getVar('guests');
 			
-			<button id="checkoutpay" class="btn btn-warning btn-lg rounded-0" data-bs-dismiss="modal-dialog">Checkout</button>
-		</div>
-    </div>
-</div>
+			// selected is any, get all the rooms/
+			if($room_type_id == '0'){
+				$data['start_date'] = $start_date;
+				$data['end_date'] = $end_date;
+				$data['room_type_id'] = $room_type_id;
+				$data['no_of_guests'] = $guests;
+				$data['rooms'] = $this->room->getAllAvailableRooms($start_date, $end_date,$guests);
+				return view('reservation/showroom',$data);
+			}
+			else{
+				$data['start_date'] = $start_date;
+				$data['end_date'] = $end_date;
+				$data['room_type_id'] = $room_type_id;
+				$data['no_of_guests'] = $guests;
+				$data['rooms'] = $this->room->getRoomsWithRoomType($start_date, $end_date, $guests, $room_type_id);
+				return view('reservation/showroom',$data);
+			}
+		}
+		return redirect()->to(base_url());
+	}
+
+	public function reserve($roomid, $startdate, $enddate, $guests){
+		$data['room'] = $this->room->getDataUsingId($roomid);
+		$data['startdate'] = $startdate;
+		$data['enddate'] = $enddate;
+		$data['guests'] = $guests;
+		return view('reservation/form', $data);
+	}
+
+	public function save($roomid, $startdate, $enddate, $guests){
+		$reservedata = [
+			'arrival_date' => $startdate,
+			'departure_date' => $enddate,
+			'no_of_guests' => $guests,
+			'room_id' => $roomid,
+		];
+		$resModel = new ReservationModel();
+		$bookModel = new BookingModel();
+		$reserve_id = $resModel->saveGetId($reservedata);
+		$bookingdata = [
+			'reservation_id' => $reserve_id,
+			'user_id' => '17', //temporary id
+		];
+		$bookingId = $bookModel->saveGetId($bookingdata);
+		return "/reservation/success/".$bookingId;
+	}
+
+	public function success($bookingId){
+		$data['bookingcode'] = password_hash($bookingId, PASSWORD_DEFAULT);
+		return view('reservation/success', $data);
+	}
+}
